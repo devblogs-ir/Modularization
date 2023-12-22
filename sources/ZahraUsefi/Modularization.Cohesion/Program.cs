9a90261da8128e879a4f81a1b7ee9c80e67c1419
Zusefi <z.usefi2020nist@gmail.com>
2023-12-22 22:53:01 +0330

    create coheision and coupling

---
+++ sources/ZahraUsefi/Modularization.Cohesion/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modularization.Cohesion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    #region Functional
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        // Other arithmetic operations
+    }
+    #endregion
+
+    #region Sequential
+    public class DataProcessor
+    {
+        public void ProcessData()
+        {
+            FetchData();
+            CleanData();
+            TransformData();
+            StoreData();
+        }
+
+        private void FetchData() { /* Fetching logic */ }
+        private void CleanData() { /* Cleaning logic */ }
+        private void TransformData() { /* Transformation logic */ }
+        private void StoreData() { /* Storage logic */ }
+    }
+
+
+    #endregion
+
+    #region Temporal
+    public class Scheduler
+    {
+        public void RunScheduledTasks()
+        {
+            Task1();
+            Task2();
+            Task3();
+            // ...
+        }
+
+        private void Task1() { /* Task 1 logic */ }
+        private void Task2() { /* Task 2 logic */ }
+        private void Task3() { /* Task 3 logic */ }
+        // ...
+    }
+
+
+    #endregion
+
+    #region Communicational
+
+    public class DataProcessor
+    {
+        private List<int> data;
+
+        public void ProcessData()
+        {
+            LoadData();
+            ProcessLoadedData();
+            // ...
+        }
+
+        private void LoadData() { /* Loading data logic */ }
+        private void ProcessLoadedData() { /* Processing logic using loaded data */ }
+        // ...
+    }
+
+    #endregion
+
+    #region Procedural
+    public class OrderProcessor
+    {
+        public void ProcessOrder(Order order)
+        {
+            ValidateOrder(order);
+            CalculateTotal(order);
+            UpdateInventory(order);
+            // ...
+        }
+
+        private void ValidateOrder(Order order) { /* Validation logic */ }
+        private void CalculateTotal(Order order) { /* Calculation logic */ }
+        private void UpdateInventory(Order order) { /* Inventory update logic */ }
+        // ...
+    }
+
+    #endregion
+
+    #region Logical
+    public class FileManager
+    {
+        public void OpenFile(string filePath)
+        {
+            // Logic to open a file
+        }
+
+        public void CloseFile(string filePath)
+        {
+            // Logic to close a file
+        }
+
+        public void ReadFile(string filePath)
+        {
+            // Logic to read a file
+        }
+
+        public void WriteFile(string filePath, string content)
+        {
+            // Logic to write to a file
+        }
+    }
+
+    #endregion
+
+    #region Coincidental
+    public class UtilityMethods
+    {
+        public void GenerateRandomNumber()
+        {
+            // Logic to generate a random number
+        }
+
+        public void SortArray(int[] array)
+        {
+            // Logic to sort an array
+        }
+
+        public void ValidateEmail(string emailAddress)
+        {
+            // Logic to validate an email address
+        }
+
+        public void CalculateArea(int length, int width)
+        {
+            // Logic to calculate the area of a rectangle
+        }
+    }
+
+    #endregion
+
+}
---
+++ sources/ZahraUsefi/Modularization.Coupling/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modularization.Coupling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    #region Content
+    public class ClassA
+    {
+        private ClassB _dependency = new ClassB();
+
+        public void DoSomething()
+        {
+            _dependency.MethodInB();
+        }
+    }
+
+    public class ClassB
+    {
+        public void MethodInB() { /* Method logic */ }
+    }
+
+    #endregion
+
+    #region Common
+    public static class GlobalData
+    {
+        public static int SharedValue { get; set; }
+    }
+
+    public class ClassA
+    {
+        public void UseSharedValue()
+        {
+            int value = GlobalData.SharedValue;
+            // Use the shared value
+        }
+    }
+
+    public class ClassB
+    {
+        public void ModifySharedValue()
+        {
+            GlobalData.SharedValue = 10;
+            // Modify the shared value
+        }
+    }
+
+    #endregion
+
+    #region Control
+    public class ClassA
+    {
+        public void DoSomething()
+        {
+            ClassB dependency = new ClassB();
+            dependency.PerformAction("actionName");
+        }
+    }
+
+    public class ClassB
+    {
+        public void PerformAction(string actionName) { /* Perform action based on the name */ }
+    }
+
+    #endregion
+
+    #region Stamp
+    public class DataProcessor
+    {
+        public void ProcessData(Data data)
+        {
+            // Use only specific fields of 'Data' object
+            int result = data.Value1 + data.Value2;
+            // ...
+        }
+    }
+
+    public class Data
+    {
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+        public int Value3 { get; set; }
+        // ...
+    }
+
+    #endregion
+
+    #region Data
+    public class ClassA
+    {
+        public void MethodA(int value)
+        {
+            ClassB dependency = new ClassB();
+            dependency.MethodB(value);
+        }
+    }
+
+    public class ClassB
+    {
+        public void MethodB(int value)
+        {
+            // Use the passed value
+        }
+    }
+
+    #endregion
+
+    #region Message
+    public class Publisher
+    {
+        public event EventHandler SomethingHappened;
+
+        public void PublishEvent()
+        {
+            SomethingHappened?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class Subscriber
+    {
+        public void Subscribe(Publisher publisher)
+        {
+            publisher.SomethingHappened += HandleEvent;
+        }
+
+        private void HandleEvent(object sender, EventArgs e)
+        {
+            // Handle the event
+        }
+    }
+
+    #endregion
+
+    #region Direct
+    public class PaymentProcessor
+    {
+        private PayPalGateway _payPalGateway;
+
+        public PaymentProcessor()
+        {
+            _payPalGateway = new PayPalGateway();
+        }
+
+        public void ProcessPayment(decimal amount)
+        {
+            // Direct usage of PayPalGateway
+            _payPalGateway.MakePayment(amount);
+        }
+    }
+
+    public class PayPalGateway
+    {
+        public void MakePayment(decimal amount)
+        {
+            // Logic to process payment via PayPal
+        }
+    }
+
+    #endregion
+
+}

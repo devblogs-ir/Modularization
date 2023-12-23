bbd025fad76f6dc394d1b354d73ad667cc9e64b5
Reza Kharaei <kharaei.developer@gmail.com>
2023-12-23 23:48:19 +0330

    Add FunctionalCohesion Class

---
+++ sources/Reza Kharaei/Design.Cohesion/FunctionalCohesion.cs
@@ -0,0 +1,28 @@
+﻿namespace Design.Cohesion.FunctionalCohesion;
+
+/* 
+    Description: 
+    this is first File.
+*/
+
+public class Calculator
+{
+    public int Add(int x, int y) => x + y;
+    
+    public int Subtract(int x, int y) => x - y;
+    
+    public int Multiply(int x, int y) => x * y;
+    
+    public double Divide(int x, int y)
+    {
+        if (y == 0)
+            throw new ArgumentException("zero is wrong!");
+
+        return (double)x / y;
+    }
+}
+
+/*
+    Description: 
+    this class including methods that related to a single purpose or functionality.
+*/

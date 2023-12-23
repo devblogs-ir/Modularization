48aa707a733786f7dac8f397172db4044a4a5af3
Reza Kharaei <kharaei.developer@gmail.com>
2023-12-23 19:12:11 +0330

    Add NoCoupling Class

---
+++ sources/Reza Kharaei/Design.Coupling/NoCoupling.cs
@@ -0,0 +1,14 @@
+﻿namespace Design.Coupling;
+
+public class NotificationService
+{
+    public void SendEmail(string Title, string Message, string strEmailAddress)
+    {
+        // Todo: send email for notification
+    }
+
+    public void SendSMS(string strMobileNumber, string Messsage)
+    {
+        // Todo : send sms for notification
+    }
+}

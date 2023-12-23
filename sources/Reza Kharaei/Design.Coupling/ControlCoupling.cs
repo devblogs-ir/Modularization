5891518ebbcc19e4c4730e3c66ec016a53bf32e2
Reza Kharaei <kharaei.developer@gmail.com>
2023-12-23 21:42:36 +0330

    Add ControlCoupling Class

---
+++ sources/Reza Kharaei/Design.Coupling/ControlCoupling.cs
@@ -0,0 +1,50 @@
+namespace Design.Coupling.ControlCoupling;
+
+/* 
+    Description: 
+    this is fourth File.
+    It is recommended to check the third file (StampCoupling.cs) before reading this file 
+*/
+
+public record NotificationData(string strType, string Title, string Message, string strEmailAddress, string strMobileNumber);
+
+public class NotificationService
+{
+    public void SendNotification(NotificationData request)
+    {
+        switch (request.strType)
+        {
+            case "SMS" :
+            {
+                // Todo: send sms for notification
+                break;
+            }
+            case "Email":
+            {
+                // Todo : send email for notification
+                break;
+            }
+        }
+    }
+}
+
+
+public class NotificationController(NotificationService notificationService)
+{
+    private readonly NotificationService _notificationService = notificationService;
+
+    public void PostAction()
+    {
+        // 1. Do something....
+
+        // 2. Send notification
+        var requestData = new NotificationData("sms", "my title", "sample message", "none@domain.com", "09119314956");
+        _notificationService.SendNotification(requestData);
+    }
+}
+
+/*
+    Description: 
+    in this example, we controlled the behavior of the SendNotification method from outside it
+    #Note: Also, this example includes Stamp Coupling...
+*/

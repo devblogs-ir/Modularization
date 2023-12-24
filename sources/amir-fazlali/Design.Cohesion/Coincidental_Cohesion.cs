3bdd65ed744d96f51d1487636d78f9d8e474684b
fzleli <amirfzlelii@gmail.com>
2023-12-24 18:06:00 +0330

    create coincidental cohesion example

---
+++ sources/amir-fazlali/Design.Cohesion/Coincidental_Cohesion.cs
@@ -0,0 +1,17 @@
+class Utilities
+{
+    public int Factorial(int n)
+    {
+        return default;
+    }
+
+    public string Reverse(string s)
+    {
+        return default;
+    }
+
+    public DateTime ShamsiToMiladi(string date)
+    {
+        return default;
+    }
+}

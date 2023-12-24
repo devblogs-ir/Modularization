3413ec5b193807f3a026ce1300725552f17ce495
sara <saragholampour971@yahoo.com>
2023-12-24 15:27:34 +0330

    add data coupling

---
+++ sources/SaraGholampour/Design.Coupling/Design.Coupling/DataCoupling.cs
@@ -0,0 +1,19 @@
+﻿namespace Design.Coupling;
+
+
+public class Student : User
+{
+    public string Grade { get; set; }
+
+    public Student(string name, byte age, string grade) : base(name, age)
+    {
+        Grade = grade;
+    }
+}
+
+
+
+public class DataCoupling
+{
+    private Student sara = new Student("sara", 20, "3");
+}
--- sources/SaraGholampour/Design.Coupling/Design.Coupling/NoDirectCoupling.cs
+++ sources/SaraGholampour/Design.Coupling/Design.Coupling/NoDirectCoupling.cs
@@ -1,6 +1,4 @@
 ﻿namespace Design.Coupling;
-
-
 public class User
 {
     public string Name { get; set; }
@@ -20,32 +18,7 @@ public class User
     }
 }
 
-public class Product
-{
-    public string Title { get; set; }
-    public uint Price { get; set; }
-    public Guid GuId { get; set; }
-
-    public Product(string title, uint price)
-    {
-        Title = title;
-        Price = price;
-        GuId = Guid.NewGuid();
-    }
-}
-
 public class NoDirectCoupling
 {
-    public User[] AllUsers =
-    {
-        new User("sara", 20),
-        new User("samira", 21),
-        new User("zahra", 22),
-    };
-
-    public Product[] products =
-    {
-        new Product("pen", 12000)
-    };
+    
 }
-

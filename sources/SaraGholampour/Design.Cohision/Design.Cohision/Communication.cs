12d9ad842806fb2851db94e677ed64cd37331138
sara <saragholampour971@yahoo.com>
2023-12-25 23:18:08 +0330

    add communication cohesion

---
+++ sources/SaraGholampour/Design.Cohision/Design.Cohision/Communication.cs
@@ -0,0 +1,23 @@
+﻿namespace Design.Cohesion;
+
+public class Communication
+{
+    public User[] HandleId(User[] users)
+    {
+        User[] newUsers = users;
+        foreach (var user in newUsers)
+        {
+            user.Id = new Guid();
+        }
+
+        return newUsers;
+    }
+
+    public User[] GetYoungUsers(User[] users)
+    {
+        User[] newUsers = users.Where(user => user.BirthDate.Year - new DateTime().Year < 25).ToArray();
+
+
+        return newUsers;
+    }
+}

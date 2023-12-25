19541ddbcb6569305fe43a5bced6db7f88611c3e
sara <saragholampour971@yahoo.com>
2023-12-25 22:22:56 +0330

    add temporal cohesion

--- sources/SaraGholampour/Design.Cohision/Design.Cohision/Logical.cs
+++ sources/SaraGholampour/Design.Cohision/Design.Cohision/Logical.cs
@@ -1,36 +1,39 @@
 ﻿namespace Design.Cohesion;
 
+public class Product
+{
+    private string Title { get; set; }
+    private ulong Price { get; set; }
+    private uint Weight { get; set; }
+
+
+    private Product(string title, ulong price, uint weight)
+    {
+        Title = title;
+        Price = price;
+        Weight = weight;
+    }
+
+    public Product SetProduct(string title, ulong price, uint weight)
+    {
+        return new Product("Book", 30000, 10);
+    }
+}
+public class TaxiService
+{
+    public string Car { get; set; }
+    public string Plaque { get; set; }
+
+    public TaxiService(string car, string plaque)
+    {
+        Car = car;
+        Plaque = plaque;
+    }
+}
+
 public class Logical
 {
-    public class Product
-    {
-        private string Title { get; set; }
-        private ulong Price { get; set; }
-        private uint Weight { get; set; }
 
 
-        private Product(string title, ulong price, uint weight)
-        {
-            Title = title;
-            Price = price;
-            Weight = weight;
-        }
 
-        public Product SetProduct(string title, ulong price, uint weight)
-        {
-            return new Product("Book", 30000, 10);
-        }
-    }
-
-    public class TaxiService
-    {
-        public string Car { get; set; }
-        public string Plaque { get; set; }
-
-        public TaxiService(string car, string plaque)
-        {
-            Car = car;
-            Plaque = plaque;
-        }
-    }
 }
---
+++ sources/SaraGholampour/Design.Cohision/Design.Cohision/Temporal.cs
@@ -0,0 +1,12 @@
+﻿namespace Design.Cohesion;
+
+public class Temporal
+{
+    public void SendDiscountCode()
+    {
+        if (new DateOnly().Day % 3 == 0)
+        {
+            Console.WriteLine();
+        }
+    }
+}

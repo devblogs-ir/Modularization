1b1d76770fa25a95f21e7daa418fcafa41131146
sara <saragholampour971@yahoo.com>
2023-12-25 11:09:10 +0330

    add control coupling

---
+++ sources/SaraGholampour/Design.Coupling/Design.Coupling/ControlCoupling.cs
@@ -0,0 +1,25 @@
+﻿namespace Design.Coupling;
+
+public class Company
+{
+    public User[] Employees { get; set; }
+    public User[] Managers { get; set; }
+
+    public void AddUserToCompany(User user, bool IsManager)
+    {
+        if (IsManager)
+            Managers = [..Managers, user];
+
+        else
+            Employees = [..Employees, user];
+    }
+}
+
+public class ControlCoupling
+{
+    public static void Main(string[] args)
+    {
+        Company DigiKala = new();
+        DigiKala.AddUserToCompany(new User("sara",20,new DateOnly()),false);
+    }
+}
